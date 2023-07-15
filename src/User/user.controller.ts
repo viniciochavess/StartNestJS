2dc3ed1227bd0d56de0ec186e98af781c5d68fb3
viniciochavess <viniciochavess@gmail.com>
2023-07-15 17:47:24 -0300

    ESTUDANDO: adicionando parametro params /user/list/:id

--- src/User/user.controller.ts
+++ src/User/user.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller()
 export class UserController {
   @Get('/user/list')
   GetUserList() {
     return 'retornar a lista de usuários';
+  }
+  @Get('/user/list/:id')
+  GetUserFindById(@Param() params:any){
+    return params.id
   }
 }
